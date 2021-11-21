c469d2bed4d7417183824906510b8f47bda78b70
OutlawJoseyWales <michaelmndz40@gmail.com>
2021-11-21 15:35:21 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Mike's NFT Collection
+
+This project was created to mint NFTs as part of my NFT Collection
+and is a practice in Solidity Development.
+
+
---
+++ scripts/run.js
@@ -0,0 +1,20 @@
+//Automatically compiles and  deploys our contracts
+
+const main = async () => {
+    const nftContractFactory = await hre.ethers.getContractFactory('MyEpicNFT');
+    const nftContract = await nftContractFactory.deploy();
+    await nftContract.deployed();
+    console.log("Contract deployed to:", nftContract.address);
+  };
+  
+  const runMain = async () => {
+    try {
+      await main();
+      process.exit(0);
+    } catch (error) {
+      console.log(error);
+      process.exit(1);
+    }
+  };
+  
+  runMain();
